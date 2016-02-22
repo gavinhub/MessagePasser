0a0582fb00075a90134885e9a0512f06de22b875
Xinghao Zhou <zhou.xinghao.1991@gmail.com>
2016-02-22 15:48:21 -0800

    debug

--- src/edu/cmu/ds/mux/CriticalSection.java
+++ src/edu/cmu/ds/mux/CriticalSection.java
@@ -2,40 +2,33 @@ package edu.cmu.ds.mux;
 
 import java.util.PriorityQueue;
 
+import edu.cmu.ds.message.util.MLogger;
+
 public class CriticalSection {
 	private String currentProcess; // Used to indicate which process are currently in CS.
-	private boolean isInCS; // Used to indicate that some process is in CS.
-	private PriorityQueue<String> queue; // Used to store the process waiting for the CS.
+	private boolean isInCs; // Used to indicate that some process is in CS.
 	
 	public CriticalSection() {
-		this.isInCS = false;
-		queue = new PriorityQueue<String>();
-		
+		this.isInCs = false;
 	}
 	
-	public void handleRequest(ResLocker locker) {
-		/* TODO: Handle the following:
-		 * 		 1. Enqueue the process when there is a process in CS.
-		 * 		 2. Tell process to send ACK.
-		 */
-		
-		if (this.isInCS) {
-			queue.add(locker.getMyName());
-		} else {
-			locker.sendAck();
-		}
-	}
-	
-	public void enterCs(String name) {
-		this.isInCS = true;
+	protected void enterCs(String name) {
+		this.isInCs = true;
+		MLogger.info("Critical Section", name + " is currently in the Critical Section");
 		currentProcess = name;
 	}
 	
-	public void leaveCs() {
-		this.isInCS = false;
+	protected void leaveCs() {
+		this.isInCs = false;
+		MLogger.info("Critical Section", this.currentProcess + " is curretnly leaving the Critical Section");
+		this.currentProcess = null;
 	}
 	
-	public String getProcessName() {
+	protected String getProcessName() {
 		return this.currentProcess;
+	}
+	
+	public boolean getIsInCs() {
+		return this.isInCs;
 	}
 }
--- src/edu/cmu/ds/mux/ResLocker.java
+++ src/edu/cmu/ds/mux/ResLocker.java
@@ -3,10 +3,12 @@ package edu.cmu.ds.mux;
 import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 import edu.cmu.ds.clock.ClockService;
 import edu.cmu.ds.message.ConfigParser;
+import edu.cmu.ds.message.util.MLogger;
 import edu.cmu.ds.multicast.GroupMessage;
 import edu.cmu.ds.multicast.MulticastMessagePasser;
 
@@ -16,92 +18,127 @@ import edu.cmu.ds.multicast.MulticastMessagePasser;
  */
 public class ResLocker extends MulticastMessagePasser implements ILocker {
 	public static enum VOTE {
-        UNVOTED, YES, NO;
-    }
-    private MulticastMessagePasser passer;
-    private String dedicatedGroup;
-    private CriticalSection cs;
-    private boolean isInCs;
-    private VOTE vote; // Is going to be deleted
-    private Set<String> ackSet;
+		UNVOTED, YES, NO;
+	}
 
-    public ResLocker(ConfigParser parser, String myName, ClockService uniClock, CriticalSection cs) throws FileNotFoundException, ParseException {
-    	super(parser, myName, uniClock);
-        this.dedicatedGroup = parser.getMuxGroup(passer.getMyName());
-        this.cs = cs;
-        this.isInCs = false;
-        this.vote = VOTE.UNVOTED;
-        this.ackSet = new HashSet<>();
-    }
+	private String dedicatedGroup;
+	private CriticalSection cs;
+	private boolean isInCs;
+	//	private VOTE vote; // Is going to be deleted
+	private Set<String> ackSet;
+	private PriorityQueue<String> queue;
 
-    /**
-     * request - request the resource. Block until the process get the resource
-     * @return true when request success. false if the process is already in the Critical Section.
-     * @throws InterruptedException 
-     */
-    @Override
-    public boolean request() throws InterruptedException {
-    	if (isInCs) {
-    		return false;
-    	}
-    	
-    	this.cs.handleRequest(this);
-    	/* 
-    	 *  Wait until getting all the ACK.
-    	 */
-    	while (this.ackSet.size() < 3) {
-    		receiveAck();
+	public ResLocker(ConfigParser parser, String myName, ClockService uniClock, CriticalSection cs) throws FileNotFoundException, ParseException {
+		super(parser, myName, uniClock);
+		this.dedicatedGroup = parser.getMuxGroup(this.getMyName());
+		this.cs = cs;
+		this.isInCs = false;
+		//    	this.vote = VOTE.UNVOTED;
+		this.ackSet = new HashSet<String>();
+		this.queue = new PriorityQueue<String>();
+	}
+
+	/**
+	 * request - request the resource. Block until the process get the resource
+	 * @return true when request success. false if the process is already in the Critical Section.
+	 * @throws InterruptedException 
+	 */
+	@Override
+	public boolean request() throws InterruptedException {
+		if (isInCs) {
+			return false;
 		}
-    	this.cs.enterCs(this.getMyName());
-    	this.isInCs = true;
-    	return true;
-    	
-    }
 
-    /**
-     * release - release the resource.
-     * @return false when not in CS; true when release successfully.
-     */
-    @Override
-    public boolean release() {
-    	this.isInCs = false;
-    	this.ackSet.clear();
-        return false;
-    }
-    
-    /**
-     *  Send ACK to all the members in the group by using R_multicast.
-     */
-    public void sendAck() {
-    	if (!isInCs) {
-    		// String src, String group, String kind, String content
-    		GroupMessage gmsg = new GroupMessage(this.getMyName(), 
-    											 this.dedicatedGroup,
-    											 "",
-    											 "ACK");
-    		R_multicast(gmsg);
-    	} 
-    }
-    
-    /**
-     *  Use R_deliver to receive ACK.
-     */
-    public void receiveAck() throws InterruptedException {
-    	GroupMessage gmsg = R_deliver();
-    	if (!gmsg.getContent().equals("ACK")) {
-    		return;
-    	} else {
-    		this.ackSet.add(gmsg.getOrigin());
-    	}
-    }
+		// String src, String group, String kind, String content
+		GroupMessage gmsg = new GroupMessage(this.getMyName(), 
+				this.dedicatedGroup,
+				"", // What the kind is?
+				"REQUEST");
+		R_multicast(gmsg);
 
-    /**
-     * Info - show information
-     *  1. how many messages have been sent or received
-     *  2. whether in CS
-     */
-    @Override
-    public void info() {
+		/* 
+		 *  Wait until getting all the ACK.
+		 */
+		while (this.ackSet.size() < 3) {
+			receiveAck();
+		}
+		this.cs.enterCs(this.getMyName());
+		this.isInCs = true;
+		return true;
 
-    }
+	}
+
+	/**
+	 * release - release the resource.
+	 * @return false when not in CS; true when release successfully.
+	 */
+	@Override
+	public boolean release() {
+		if (!this.isInCs) {
+			return false;
+		}
+		this.isInCs = false;
+		this.ackSet.clear();
+		this.cs.leaveCs();
+		return true;
+	}
+
+	/**
+	 * handleRequest - Handle the received requests sent by other processes.
+	 */
+	public void handleRequest() throws InterruptedException {
+		/* TODO: Handle the following:
+		 * 		 1. Enqueue the process when there is a process in CS.
+		 * 		 2. Tell process to send ACK.
+		 */
+		GroupMessage gmsg = R_deliver();
+		if (!gmsg.getContent().equals("REQUEST")) {
+			return;
+		}
+
+		if (this.isInCs) {
+			queue.add(gmsg.getSourceName());
+		} else {
+			sendAck();
+		}
+	}
+
+
+	/**
+	 *  Send ACK to all the members in the group by using R_multicast.
+	 */
+	public void sendAck() {
+		if (!isInCs) {
+			// String src, String group, String kind, String content
+			GroupMessage gmsg = new GroupMessage(this.getMyName(), 
+					this.dedicatedGroup,
+					"", // What the kind is?
+					"ACK");
+			R_multicast(gmsg);
+		} 
+	}
+
+	/**
+	 *  Use R_deliver to receive ACK.
+	 */
+	public void receiveAck() throws InterruptedException {
+		GroupMessage gmsg = R_deliver();
+		if (gmsg.getContent().equals("ACK")) {
+			this.ackSet.add(gmsg.getOrigin());
+		}
+	}
+
+	/**
+	 * Info - show information
+	 *  1. how many messages have been sent or received
+	 *  2. whether in CS
+	 */
+	@Override
+	public void info() {
+		if (cs.getIsInCs()) {
+			MLogger.info("Critical Section", cs.getProcessName() + " is currently in Critical Section");
+		} else {
+			
+		}
+	}
 }
